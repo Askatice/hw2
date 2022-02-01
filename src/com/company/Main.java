d721b7a1d27868f6c09e1891201c36ba559a2511
Askat Iliasbekov <97965896+Askatice@users.noreply.github.com>
2022-02-01 19:04:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner word = new Scanner(System.in);
+        String first;
+        System.out.println("Ваше имя: ");
+        first = word.nextLine();
+        System.out.println("Добро пожаловать, " + first);
+
+        int second;
+        System.out.println("Ваш возраст: ");
+        second = word.nextInt();
+        if (second <= 17){
+            System.out.println("Вы должны быть совершено летным");
+        } else {
+            System.out.println("Вы успешно зашли, " + first);
+        }
+        String third;
+        System.out.println("Введите ваш пароль: ");
+        third = word.nextLine();
+        System.out.println("пароль верный!" + third);
+        }
+    }
+
